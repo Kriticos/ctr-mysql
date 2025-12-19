085ccd07fb35e9db708f1b21c37c8486e4091c34
Kriticos <kriticos@gmail.com>
2025-12-19 14:06:58 -0300

    * feat(my.cnf): added 'log_bin_trust_function_creators = 1' * fix(02-zabbix.sh): corrected 'collate' to 'COLLATE'

--- mysql-init/02-zabbix.sh
+++ mysql-init/02-zabbix.sh
@@ -6,14 +6,12 @@ echo "▶ Criando banco e usuário do Zabbix..."
 mysql -uroot -p"$MYSQL_ROOT_PASSWORD" <<EOF
 CREATE DATABASE IF NOT EXISTS zabbix
   CHARACTER SET utf8mb4
-  collate utf8mb4_bin;
+  COLLATE utf8mb4_bin;
 
 CREATE USER IF NOT EXISTS '$MYSQL_ZBX_USER'@'%'
   IDENTIFIED BY '$MYSQL_ZBX_PASSWORD';
 
 GRANT ALL PRIVILEGES ON zabbix.* TO '$MYSQL_ZBX_USER'@'%';
-
-SET GLOBAL log_bin_trust_function_creators = 1;
 
 FLUSH PRIVILEGES;
 EOF
