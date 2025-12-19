3ea60e9ef3f6a96a20550650b52f7d1ac22fe76c
Kriticos <kriticos@gmail.com>
2025-12-18 22:23:32 -0300

    * feat(mysql-init): set global log_bin_trust_function_creators to 1

--- mysql-init/02-zabbix.sh
+++ mysql-init/02-zabbix.sh
@@ -6,12 +6,14 @@ echo "▶ Criando banco e usuário do Zabbix..."
 mysql -uroot -p"$MYSQL_ROOT_PASSWORD" <<EOF
 CREATE DATABASE IF NOT EXISTS zabbix
   CHARACTER SET utf8mb4
-  COLLATE utf8mb4_unicode_ci;
+  collate utf8mb4_bin;
 
 CREATE USER IF NOT EXISTS '$MYSQL_ZBX_USER'@'%'
   IDENTIFIED BY '$MYSQL_ZBX_PASSWORD';
 
 GRANT ALL PRIVILEGES ON zabbix.* TO '$MYSQL_ZBX_USER'@'%';
+
+SET GLOBAL log_bin_trust_function_creators = 1;
 
 FLUSH PRIVILEGES;
 EOF
